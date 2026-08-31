3f6ad1e1d5282972db33edc7b2e53fdaf1b6678e
Amaryt1 <am822ar@gmail.com>
2026-08-31 08:44:08 +0300

    Add AM API config

---
+++ am-api/config.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+const DB_HOST = 'localhost';
+const DB_NAME = 'if0_42375341_amapi';
+const DB_USER = 'if0_42375341';
+const DB_PASS = 'PUT_YOUR_MYSQL_PASSWORD_HERE';
+
+const API_NAME = 'AM API';
+const API_VERSION = '1.0.0';
+const TOKEN_EXPIRE_DAYS = 30;
